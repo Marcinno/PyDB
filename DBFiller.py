0f856e0d7c211ce04ffccab1bfe5201625daa560
Marcinno <marrcinek96@gmail.com>
2019-03-27 08:15:14 +0100

    refactored code & invoking method from list bug

--- DBFiller.py
+++ DBFiller.py
@@ -3,25 +3,24 @@ import datetime
 import random
 import string
 import time
-sql = """SELECT
-object_name(parent_object_id),
-object_name(referenced_object_id),
-name 
-FROM sys.foreign_keys
-WHERE parent_object_id = object_id('Kraj')
 
-"""
+class Settinger():
+    def __init__():
+        pass
 class DBdriver():
     def __init__(self):
-        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-137JTOH\SQLEXPRESS;DATABASE=Test;UID=user;PWD=admin') #hard coded connection string
-        cursor = conn.cursor()
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-137JTOH\SQLEXPRESS;DATABASE=Test;UID=user;PWD=admin') #hard coded connection string
+        cursor = connection.cursor()
+        method_returning_value_list = ["","","","get_decimal","get_int","","","","","get_date","","","get_varchar"]
 
-        self.conn = conn
+        self.connection = connection
         self.cursor = cursor
         self.table_names = []     
+        self.method_returning_value_list = method_returning_value_list
+        
 
     def get_table_list(self):
-        self.d = dict()
+        self.settings_dict = dict()
         self.cursor.execute("SELECT * FROM Sys.Tables")
         row = self.cursor.fetchall()
         print("Printing list of database tables")
@@ -32,12 +31,11 @@ class DBdriver():
         for i in self.table_names:
             print("Settings : %r" %i)
             value = input("Podaj wartość : ")
-            self.d[i] = value
+            self.settings_dict[i] = value
 
-        self.filling_itxd()
+        self.get_extra_info()
 
-    def filling_itxd(self):
-        global sql
+    def get_extra_info(self):
         for i in self.table_names:
 
             self.cursor.execute("SELECT object_name(parent_object_id), object_name(referenced_object_id), name FROM sys.foreign_keys WHERE parent_object_id = object_id('{0}')".format(i))
@@ -46,81 +44,47 @@ class DBdriver():
             self.cursor.execute("sp_Columns '"+i+"' ")
             colums = self.cursor.fetchall()
             
-            if not fk_tables:
-                self.insert_data(i, colums)
-            else:
-                self.insert_data_with_fk(i, colums, fk_tables)
-
-    def insert_data(self, table_name, columns):
-        for a in range(int(self.d[table_name])):              
-            params = []
-            print(len(columns))
-
-            for i in columns:
-                if i[4] == 4:
-                    ina = random.randrange(0, 101, 2)
-                    params.append(ina)
-                elif i[4] == 12:
-                    sta = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
-                    params.append(sta)
-                elif i[4] == 3:
-                    params.append(random.randrange(0, 101, 2))           
-                elif i[4] == -9:
-                    params.append(datetime.datetime.now)
-            param = params[1:len(params)]
-            #print(param)
-            query = "INSERT INTO {0} values ({1})".format(table_name, ','.join('?' * (len(columns) -1) ))
-            self.cursor.execute(query,param)
-            self.conn.commit()
+            self.insert_data_with_fk(i, colums, fk_tables)
 
     def insert_data_with_fk(self, table_name, colums, fk_tables):
-        d1 = dict()
-        l1 = []
-        for i in fk_tables:
-            temp = i[2]
-            cut =  i[2][:3] + i[2][(len(table_name) + 4):]
-            #print(cut)
-            if len(cut) < 4:
-                d1[i[2].lower()] = i[1].lower()
-                l1.append(i[2])
-            else:
-                d1[cut.lower()] = i[1].lower()
-                l1.append(cut)
+        related_table_with_keys = dict()
+        fk_names_list = []
+        if len(fk_tables) != 0 :              
 
-        l2 = [x.lower() for x in l1]
-        for a in range(int(self.d[table_name])):              
-            params = []
-
-            print(l1)
-            for i in colums:
-                #time.sleep(0.1)
-                print("KII " + i[3].lower())
-                if i[3].lower() in l2:
-                    print("????")
-                    x = self.get_count(d1[i[3].lower()])
-                    print("Wartość %x" %x)
-                    params.append(x)
+            for i in fk_tables:
+                cut =  i[2][:3] + i[2][(len(table_name) + 4):]
+                #print(cut)
+                if len(cut) < 4:
+                    related_table_with_keys[i[2].lower()] = i[1].lower()
+                    fk_names_list.append(i[2].lower())
                 else:
-                    if i[4] == 4:
-                        ina = random.randrange(1, 80, 1)
-                        params.append(ina)
-                    elif i[4] == 12:
-                        sta = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(6))
-                        params.append(str(sta))
-                    elif i[4] == 3:
-                        params.append(25.5)           
-                    elif i[4] == -9:
-                        params.append("2005-02-02")
-            param = params[1:len(params)]
-            print(param)
-            print(colums[0][3].lower())
+                    related_table_with_keys[cut.lower()] = i[1].lower()
+                    fk_names_list.append(cut.lower())
+
+        #l2 = [x.lower() for x in l1]
+
+        for a in range(int(self.settings_dict[table_name])):
+            parameters_value = []          
+            for i in colums:
+                if i[3].lower() in fk_names_list:
+                    x = self.get_count(related_table_with_keys[i[3].lower()])
+                    print("Wartość %x" %x)
+                    parameters_value.append(x)
+                else:
+                    #for a in globals().keys():
+                        #print(a)
+                    #locals()["DBdriver"][self.method_returning_value_list[abs(i[4])]]()
+                    DBdriver.self.method_returning_value_list[i[4]]()
+
+            cuted_parameter_value = parameters_value[1:len(parameters_value)] # cuted because public key isn't required to insert
+
             if (colums[0][3].lower() == 'pk_id'):
                 query = "INSERT INTO {0} values ({1})".format(table_name, ','.join('?' * (len(colums) -1) ))
-                self.cursor.execute(query,param)
+                self.cursor.execute(query,cuted_parameter_value)
             else:
                 query = "INSERT INTO {0} values ({1})".format(table_name, ','.join('?' * len(colums) ))
-                self.cursor.execute(query,params)
-            self.conn.commit()       
+                self.cursor.execute(query,parameters_value)
+            self.connection.commit()       
 
     def get_count(self, name):
         self.cursor.execute("SELECT count(*) FROM dbo."+name+" ")
@@ -128,12 +92,16 @@ class DBdriver():
         print(row[0])
         return row[0]
 
-class FillData():
-    def __init__():
+    def get_nvarchar(self):
         pass
-
-
-#cursor.execute(sql)
+    @staticmethod
+    def get_int():
+        print("dupa")
+        pass
+    def get_date(self):
+        pass
+    def get_money(self):
+        pass
 
 
 if __name__ == "__main__":
